e33c38880d06ae6b01f2cd7bad70ad2cc595de05
OantiO <97236521+OantiO@users.noreply.github.com>
2024-04-07 09:34:09 +0200

    Add files via upload

--- finder.mjs
+++ finder.mjs
@@ -1,15 +1,20 @@
 function finder() {
     // put your words in this arry (lost)
-    const lost = ["c","a"]
-    const words = ["a", "a", "b", "c", "d", "e", "e", "f", "g", "g", "h", "i", "i", "j", "k", "l", "m", "n", "o", "o", "p", "q", "q", "r"]
-  
+    const lost = ["j","a","o","e"]
+    const words = ["a", "a", "b", "c", "a", "e", "e", "f", "g", "g", "h", "i", "i", "j", "k", "l", "m", "n", "o", "o", "p", "q", "q", "r"]
+    let sumer =0
     for (let s = 0; s < lost.length; s++) {
       for (let r = 0; r < words.length; r++) {
         if (words[r] == lost[s]) {
+          sumer = sumer+1
+          if (sumer == 1)
+          {
           console.log(words[r])
-          console.log(r)
-        }
+          }
+        }    
       }
+      console.log(sumer)
+      sumer = 0
     }
   }
   finder()
